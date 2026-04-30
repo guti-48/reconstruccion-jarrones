af33768e906deda79e674ac26e93194c5170de71
Rafaelanaoro <91906366+Rafaelanaoro@users.noreply.github.com>
2026-04-30 19:54:40 +0200

    Arreglos

--- training/train_gan.py
+++ training/train_gan.py
@@ -12,7 +12,7 @@ from utils.metrics import EdgeAccuracy
 class Config:
     # Parámetros del entrenamiento
     PATH = './checkpoints'
-    DATA_ROOT = '/content/data_rapida/processed'
+    DATA_ROOT = '/content/data_rapida/processed' #Ruta Google Colab
     GAN_LOSS = 'nsgan'
     LR = 0.0001
     D2G_LR = 0.1
@@ -33,15 +33,14 @@ def train_gan():
 
     print("--- Iniciando Entrenamiento de Patrones (Edge GAN) ---")
     print(f"Usando dispositivo: {config.DEVICE}")
-
-    # Cargar el Dataset y Modelo
+    
     train_dataset = CeramicDataset(root_dir=config.DATA_ROOT)
     train_loader = DataLoader(dataset=train_dataset, batch_size=config.BATCH_SIZE, shuffle=True, drop_last=True)
     
     edge_model = EdgeModel(config).to(config.DEVICE)
     edgeacc = EdgeAccuracy(config.EDGE_THRESHOLD).to(config.DEVICE)
 
-    # --- LÓGICA DE CARGA DE PESOS (RETOMAR ENTRENAMIENTO) ---
+    # Retormar entrenamientos
     if config.START_EPOCH > 1:
         print(f"Buscando pesos previos para retomar en la época {config.START_EPOCH}...")
         
@@ -49,25 +48,23 @@ def train_gan():
         ruta_dis = os.path.join(config.PATH, config.DIS_WEIGHTS)
         
         if os.path.exists(ruta_gen) and os.path.exists(ruta_dis):
-            # 1. Cargamos los "maletines" enteros a la memoria
+            # Cargamos los "maletines" enteros a la memoria
             checkpoint_gen = torch.load(ruta_gen, map_location=config.DEVICE)
             checkpoint_dis = torch.load(ruta_dis, map_location=config.DEVICE)
             
-            # 2. Extraemos SOLO los pesos reales ignorando el número de iteración
+            # Extraemos SOLO los pesos reales ignorando el número de iteración
             pesos_gen = checkpoint_gen['generator'] if 'generator' in checkpoint_gen else checkpoint_gen
             
             # (Hacemos lo mismo para el discriminador, asumiendo que su clave se llama así)
             pesos_dis = checkpoint_dis['discriminator'] if 'discriminator' in checkpoint_dis else checkpoint_dis
 
-            # 3. Le inyectamos los pesos limpios a los modelos
+            # Le inyectamos los pesos limpios a los modelos
             edge_model.generator.load_state_dict(pesos_gen)
             edge_model.discriminator.load_state_dict(pesos_dis)
             
-            print("¡Memoria antigua inyectada con éxito! La IA recuerda su entrenamiento.")
+            print("Memoria antigua inyectada con éxito. La IA recuerda su entrenamiento.")
         else:
-            print("⚠️ ADVERTENCIA: No se encontraron los archivos .pth en la carpeta checkpoints.")
-            print("Asegúrate de que los nombres GEN_WEIGHTS y DIS_WEIGHTS son correctos.")
-            print("El entrenamiento va a empezar desde cero por seguridad.")
+            print("Error: No se encontraron los archivos .pth en la carpeta checkpoints.")
             config.START_EPOCH = 1
     else:
         print("Empezando entrenamiento desde cero...")
@@ -100,7 +97,6 @@ def train_gan():
         print(f" -> Precisión de Bordes: {(epoch_precision/num_batches)*100:.2f}%")
         
         if epoch % 10 == 0:
-            # Tu método save() de EdgeModel ya sobrescribe los archivos con el progreso actual
             edge_model.save()
             print(f"--> Punto de control guardado en la época {epoch}")
 
